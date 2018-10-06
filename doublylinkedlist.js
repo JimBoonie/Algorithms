49599b2f887e934db80a96ba8052b25122286ef6
Mason McGough <mcgough.mason@gmail.com>
2018-10-06 15:49:57 -0400

    Implement doubly linked list as class;

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -1,39 +1,41 @@
-var doublyLinkedListNode = (val, prev, next) => ({
-    val: val,
-    prev: prev,
-    next: next,
-});
-
-var doublyLinkedList = function(arr) {
-    var first = doublyLinkedListNode(arr[0], null, null);
-    var prev = first;
-    for (let i = 1; i < arr.length; i++) {
-        var node = doublyLinkedListNode(arr[i], prev, null);
-        prev.next = node;
-        prev = node;
+class DoublyLinkedList {
+    constructor(arr) {
+        var first = this.doublyLinkedListNode(arr[0], null, null);
+        var prev = first;
+        for (let i = 1; i < arr.length; i++) {
+            var node = this.doublyLinkedListNode(arr[i], prev, null);
+            prev.next = node;
+            prev = node;
+        }
+    
+        this.node = first;
     }
 
-    return first;
+    doublyLinkedListNode(val, prev, next) {
+        return {
+            val: val,
+            prev: prev,
+            next: next,
+        }
+    }
+
+    walkForward() {
+        console.log(this.node.val);
+        while (this.node.next != null) {
+            this.node = this.node.next;
+            console.log(this.node.val);
+        } 
+    }
+
+    walkBackward() { 
+        console.log(this.node.val);
+        while (this.node.prev != null) {
+            this.node = this.node.prev;
+            console.log(this.node.val);
+        } 
+    }
 }
 
-var walkForward = function(ll) {
-    do {
-        console.log(ll.val);
-        ll = ll.next;
-    } while (ll != null)
-}
-
-var walkBackward = function(ll) {
-    do {
-        ll = ll.next;
-    } while(ll.next != null)
-
-    do {
-        console.log(ll.val);
-        ll = ll.prev;
-    } while (ll != null)
-}
-
-var linked_list = doublyLinkedList([0, 1, 2, 3, 4]);
-walkForward(linked_list);
-walkBackward(linked_list);
+var linked_list = new DoublyLinkedList([0, 1, 2, 3, 4]);
+linked_list.walkForward();
+linked_list.walkBackward();
